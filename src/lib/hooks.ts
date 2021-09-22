e90151aff73cd7bed8c3920a3425bce66802d399
Jateen Patel <jpatel01@tucows.com>
2021-09-22 15:21:20 -0300

    adding vault integration

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -25,7 +25,7 @@ function createTestFile() {
 function runTest(browser) {
   let runner
 
-  return testCafe('localhost').then(function(tc: any) {
+  return testCafe('localhost').then(function (tc: any) {
     cafeRunner = tc
     runner = tc.createRunner()
 
@@ -33,31 +33,52 @@ function runTest(browser) {
       .src(RUNNER_FILE)
       .screenshots({
         path: `${process.env.CUCUMBER_REPORTS}/screenshots/`,
-        takeOnFails: true
+        takeOnFails: true,
       })
       .browsers(browser || 'chrome')
       .run({
         skipJsErrors: true,
         selectorTimeout: TIMEOUT / 2,
-        assertionTimeout: TIMEOUT * 0.9
+        assertionTimeout: TIMEOUT * 0.9,
       })
   })
 }
 
 setDefaultTimeout(TIMEOUT)
 
-BeforeAll(function() {
-  createTestFile()
-  runTest(process.env.CUCUMBER_BROWSER || this.parameters.browser)
+async function setVaultToken() {
+  const Vault = require('hashi-vault-js')
+  const vaultConfig = require('../../../../configs/vaultConfig').default
+  const vault = new Vault(vaultConfig)
+  // Only request new vault token if not already retrieved or older than 4 hours 
+  if (
+    !process.env.VAULT_TOKEN ||
+    (process.env.VAULT_TOKEN &&
+      new Date().getTime() / 1000 -
+        parseInt(process.env.TOKEN_LAST_RETRIEVED) >=
+        14400)
+  ) {
+    const readlineSync = require('readline-sync')
+    const userName = readlineSync.question('Enter LDAP username: ')
+    const passwd =readlineSync.question('Enter LDAP password: ')
+    const token = await vault.loginWithLdap(userName, passwd)
+    process.env.VAULT_TOKEN= token.client_token
+    process.env.TOKEN_LAST_RETRIEVED = `${(new Date().getTime() / 1000)}`
+}
+}
 
-  return testControllerHolder.get().then(t => t.maximizeWindow())
+BeforeAll(async function () {
+  createTestFile()
+  await setVaultToken()
+  runTest(process.env.CUCUMBER_BROWSER || this.parameters.browser)
+  return testControllerHolder.get().then((t) => t.maximizeWindow())
 })
 
 export function resetBrowser(t) {
   return ClientFunction(() => {
     localStorage.clear()
     sessionStorage.clear()
-    return document.cookie.split(';').forEach(function(c) {
+    return document.cookie.split(';').forEach(function (c) {
       document.cookie = c
         .replace(/^ +/, '')
         .replace(/=.*/, '=;expires=' + new Date().toUTCString() + ';path=/')
@@ -65,7 +86,7 @@ export function resetBrowser(t) {
   }).with({ boundTestRun: t })()
 }
 
-After(async function(testCase) {
+After(async function (testCase) {
   const world = this
   if (testCase.result.status === Status.FAILED) {
     attachScreenshotToReport = world.attachScreenshotToReport
@@ -76,34 +97,33 @@ After(async function(testCase) {
   return testControllerHolder.get().then(resetBrowser)
 })
 
-AfterAll(async function() {
+AfterAll(async function () {
   if (existsSync(RUNNER_FILE)) unlinkSync(RUNNER_FILE)
   await testControllerHolder.free()
   return cafeRunner.close()
 })
 
-const getAttachScreenshotToReport = path => {
+const getAttachScreenshotToReport = (path) => {
   return attachScreenshotToReport(path)
 }
 
-const addErrorToController = async resolvedTestController => {
-  return resolvedTestController.executionChain.catch(result => {
-    const errAdapter = new testCafe.embeddingUtils.TestRunErrorFormattableAdapter(
-      result,
-      {
+const addErrorToController = async (resolvedTestController) => {
+  return resolvedTestController.executionChain.catch((result) => {
+    const errAdapter =
+      new testCafe.embeddingUtils.TestRunErrorFormattableAdapter(result, {
         testRunPhase: resolvedTestController.testRun.phase,
         userAgent:
-          resolvedTestController.testRun.browserConnection.browserInfo.userAgent
-      }
-    )
+          resolvedTestController.testRun.browserConnection.browserInfo
+            .userAgent,
+      })
     return resolvedTestController.testRun.errs.push(errAdapter)
   })
 }
 
-const ifErrorTakeScreenshot = async resolvedTestController => {
+const ifErrorTakeScreenshot = async (resolvedTestController) => {
   if (resolvedTestController.testRun.opts.screenshots.takeOnFails) {
     resolvedTestController.executionChain._state = 'fulfilled'
-    return resolvedTestController.takeScreenshot().then(path => {
+    return resolvedTestController.takeScreenshot().then((path) => {
       return getAttachScreenshotToReport(path)
     })
   } else {
